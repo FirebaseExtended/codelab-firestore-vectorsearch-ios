315bb21acc118b5085abaef52a354aa818fd3f50
Peter Friese <peter@peterfriese.de>
2024-04-10 15:26:54 +0200

    📝 Fix copyright headers
    Signed-off-by: Peter Friese <peter@peterfriese.de>

--- code/frontend/Notes/Notes/NotesApp.swift
+++ code/frontend/Notes/Notes/NotesApp.swift
@@ -1,8 +1,5 @@
 //
-// NotesApp.swift
-// Notes
-//
-// Created by Peter Friese on 08.04.24.
+// Copyright © 2024 Google LLC. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
