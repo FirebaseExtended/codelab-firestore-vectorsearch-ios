952cf3991c2d9bff479cbae7be2fcade8823d939
Peter Friese <peter@peterfriese.de>
2024-04-11 16:49:42 +0200

    📝 Fix copyright headers
    Signed-off-by: Peter Friese <peter@peterfriese.de>

--- code/frontend/Notes/Notes/Views/NoteRowView.swift
+++ code/frontend/Notes/Notes/Views/NoteRowView.swift
@@ -1,8 +1,5 @@
 //
-// NoteRowView.swift
-// Notes
-//
-// Created by Peter Friese on 09.04.24.
+// Copyright © 2024 Google LLC. All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
